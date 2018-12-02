31237afa1766cd0f7abc8e3e22524ebe8ed833c1
Matt Stave <mstave@gmail.com>
2018-12-02 08:59:25 -0600

    Breakout All view

---
+++ src/main/kotlin/kgui/view/All.kt
@@ -0,0 +1,40 @@
+package kgui.view
+
+import javafx.geometry.Orientation
+import javafx.scene.layout.Priority
+import javafx.scene.layout.VBox
+import kgui.app.PicCollectionsController
+import kpics.AbstractPicCollection
+import tornadofx.*
+
+class All : View("All") {
+    override val root = splitpane(Orientation.HORIZONTAL)
+    private val pCont: PicCollectionsController by param()
+
+    init {
+        with(root) {
+            prefWidth = 1200.0
+            minHeight = 500.0
+            for (v in pCont.allPicLibs) {
+                this += find<PicsFragment>(mapOf(PicsFragment::picObj to v))
+            }
+            // even out dividers, weird that this seems to be needed
+            for (i in 0 until dividers.size) {
+                dividers[i].position = (1.0 + i) / (dividers.size + 1)
+            }
+        }
+    }
+}
+
+class PicsFragment : Fragment() {
+    val picObj: AbstractPicCollection by param()
+    private val lview = listview<String> {
+        prefWidth = 300.0
+        minHeight = 600.0
+        items = picObj.relativePaths.toList().observable()
+        selectionModel.select(0)
+        vgrow = Priority.ALWAYS
+    }
+    override val root = VBox(picObj.baseStr?.let { label(it) }, lview,
+                                                 label("Count: " + picObj.count + "\tName: " + picObj.toString()))
+}
--- src/main/kotlin/kgui/view/MainView.kt
+++ src/main/kotlin/kgui/view/MainView.kt
@@ -1,10 +1,6 @@
 package kgui.view
 
-import javafx.geometry.Orientation
-import javafx.scene.layout.Priority
-import javafx.scene.layout.VBox
 import kgui.app.PicCollectionsController
-import kpics.AbstractPicCollection
 import mu.KLogger
 import mu.KotlinLogging
 import tornadofx.*
@@ -22,17 +18,7 @@ class MainView : View("Pics") {
                 this += find<DBForm>(DBForm::pCont to controller)
             }
             tab("All") {
-                splitpane(Orientation.HORIZONTAL) {
-                    prefWidth = 1200.0
-                    minHeight = 500.0
-                    for (v in controller.allPicLibs) {
-                        this += find<PicsFragment>(mapOf(PicsFragment::picObj to v))
-                    }
-                    // even out dividers, weird that this seems to be needed
-                    for (i in 0 until dividers.size) {
-                        dividers[i].position = (1.0 + i) / (dividers.size + 1)
-                    }
-                }
+                this += find<All>(DBForm::pCont to controller)
             }
             tab("differences") {
                 this += find<Diffs>(Diffs::pCont to controller)
@@ -52,18 +38,5 @@ class MainView : View("Pics") {
 class AddDirDialogView : UIComponent() {
     override val root = form {
     }
-}
-
-class PicsFragment : Fragment() {
-    val picObj: AbstractPicCollection by param()
-    private val lview = listview<String> {
-        prefWidth = 300.0
-        minHeight = 600.0
-        items = picObj.relativePaths.toList().observable()
-        selectionModel.select(0)
-        vgrow = Priority.ALWAYS
-    }
-    override val root = VBox(picObj.baseStr?.let { label(it) }, lview,
-                             label("Count: " + picObj.count + "\tName: " + picObj.toString()))
 }
 
